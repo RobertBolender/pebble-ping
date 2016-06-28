7ff9e1cf4ceb287f5b9f78a424e526c72cd6fa51
Bob Bolender <bobthethree@gmail.com>
2016-06-28 08:04:27 -0500

    Make Pebble Ping Playable

--- src/js/app.js
+++ src/js/app.js
@@ -6,83 +6,136 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Settings = require('settings');
+
+// Initial settings
+
+Settings.data('player1', 'Player 1');
+Settings.data('player1Score', 0);
+Settings.data('player2', 'Player 2');
+Settings.data('player2Score', 0);
+
+var getBody = function(){
+  var data = Settings.data();
+  return data.player1 + ': ' + data.player1Score + "\n" + data.player2 + ': ' + data.player2Score;
+}
+
+var gameOver = function(winner){
+  var card = new UI.Card();
+  var data = Settings.data();
+  card.title('Game Over');
+  card.subtitle(winner + ' wins!');
+  card.body(getBody());
+  card.show();
+}
+
+var checkScore = function(){
+  var player1Score = Settings.data('player1Score');
+  var player2Score = Settings.data('player2Score');
+  var highScore = 0, lowScore = 0;
+  var winning = '';
+  if (player1Score === player2Score) return;
+  if (player1Score > player2Score){
+    winning = Settings.data('player1');
+    highScore = player1Score;
+    lowScore = player2Score;
+  } else {
+    winning = Settings.data('player2');
+    highScore = player2Score;
+    lowScore = player1Score;
+  }
+
+  // Skunk
+  if (highScore === 7 && lowScore === 0){
+    gameOver(winning);
+  }
+
+  // Normal win
+  if (highScore >= 11 && (highScore - lowScore) >= 2){
+    gameOver(winning);
+  }
+
+}
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'Pebble Ping',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
+  body: getBody(),
   subtitleColor: 'indigo', // Named colors
   bodyColor: '#9a0036' // Hex colors
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
+main.on('longClick', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        title: 'Player 1'
       }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
+        title: 'Bob'
       }, {
-        title: 'Third Item',
+        title: 'Jacob'
       }, {
-        title: 'Fourth Item',
+        title: 'Timothy'
+      }, {
+        title: 'Guilherme'
       }]
     }]
   });
   menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+    console.log('Selected item #' + e.itemIndex);
+    console.log('The selection is titled "' + e.item.title + '"');
+    Settings.data('player1', e.item.title);
+    main.body(getBody());
+    main.show();
   });
   menu.show();
 });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
+main.on('longClick', 'down', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Player 2'
+      }, {
+        title: 'Bob'
+      }, {
+        title: 'Jacob'
+      }, {
+        title: 'Timothy'
+      }, {
+        title: 'Guilherme'
+      }]
+    }]
   });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex);
+    console.log('The selection is titled "' + e.item.title + '"');
+    Settings.data('player2', e.item.title);
+    main.body(getBody());
+    main.show();
   });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
+  menu.show();
+});
+
+main.on('click', 'up', function(e) {
+  Settings.data('player1Score', Settings.data('player1Score') + 1);
+  main.body(getBody());
+  checkScore();
 });
 
 main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  Settings.data('player2Score', Settings.data('player2Score') + 1);
+  main.body(getBody());
+  checkScore();
 });
+
+main.on('longClick', 'select', function(e) {
+  Settings.data({
+    player1Score: 0,
+    player2Score: 0
+  });
+  main.body(getBody());
+});
+
